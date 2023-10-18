6ffa7ed701ca2ef3557172aa33e68d9654df2d86
adenugbamichael <adenugbamicke@gmail.com>
2023-10-18 08:41:01 +0100

    Movie list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import Billboard from "@/components/Billboard"
-import Navbar from "@/components/Navbar"
-import useCurrentUser from "@/hooks/useCurrentUser"
+import React from "react"
 import { NextPageContext } from "next"
-import { getSession, signOut } from "next-auth/react"
+import { getSession } from "next-auth/react"
+
+import Navbar from "@/components/Navbar"
+import Billboard from "@/components/Billboard"
+import MovieList from "@/components/MovieList"
+import useMovieList from "@/hooks/useMovieList"
+import useFavorites from "@/hooks/useFavorites"
+import useInfoModalStore from "@/hooks/useInfoModalStore"
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -22,11 +27,17 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser()
+  const { data: movies = [] } = useMovieList()
+  const { data: favorites = [] } = useFavorites()
+  const { isOpen, closeModal } = useInfoModalStore()
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+        <MovieList title='My List' data={favorites} />
+      </div>
     </>
   )
 }
